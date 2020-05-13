07afe722714f2575628bc320d930dc3b807b1980
Hnin Wai Wai Hlaing <hninwaiwaihlaing1906@gmail.com>
2020-05-13 13:40:51 +0630

    initial commit

---
+++ src/main/java/guru/springframework/controller/AuthorController.java
@@ -0,0 +1,24 @@
+package guru.springframework.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.repository.AuthorRepository;
+
+@Controller
+public class AuthorController {
+	
+	private final AuthorRepository authorRepository ;
+	
+	
+	public AuthorController(AuthorRepository authorRepository) {
+		this.authorRepository = authorRepository;
+	}
+
+	@RequestMapping("/authors")
+	public String getAuthors(Model model) {
+		model.addAttribute("authors", authorRepository.findAll());
+		return "author_list";
+	}
+}
