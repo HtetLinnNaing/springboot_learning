b8b86108cac8695357cc92b1919651b6298e50c8
HtetLinnNaing <htetlinng@isgm2.com>
2020-05-14 10:31:12 +0630

    ===first commit

--- src/main/java/guru/springframework/controller/AuthorController.java
+++ src/main/java/guru/springframework/controller/AuthorController.java
@@ -3,10 +3,12 @@ package guru.springframework.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import guru.springframework.repository.AuthorRepository;
 
 @Controller
+@RestController("/")
 public class AuthorController {
 	
 	private final AuthorRepository authorRepository ;
